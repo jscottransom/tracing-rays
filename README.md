2e94e589f2ce339ef850abc42667605e9853b61d
jscottransom <james.scottran@gmail.com>
2022-12-27 10:26:42 -0800

    base PPM images

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tracing-rays
 Ray Tracer Implementation in Rust &amp; Python
+
+# Method
+This repository contains Pythonic and Rusty implementations of the Ray Tracing Algorithm. Each folder focuses on using the strengths of the language to produce the resulting images.
+
+# Viewing
+For .ppm files, please view in a PPM viewer.
---
+++ pytrace/image_dev.py
@@ -0,0 +1,32 @@
+def build_image():
+    """
+    Simple functio to generate a static ppm file.
+
+    To call this and export to a ppm file, run python image_dev.py > {file_name}.ppm
+    Then, open the PPM in a viewer to review the image.
+    
+    """
+
+    # Construct image Specs
+
+    image_width = 256
+    image_height = 256
+
+    # Render Image
+
+    print(f"P3\n{image_width} {image_height} \n255\n")
+
+    for num in range(image_height-1, -1, -1):
+        for secnum in range(0, image_width, 1):
+            r = float(secnum)/ (image_width - 1)
+            g = float(num) / (image_height - 1)
+            b = 0.25
+
+            ir = int(255.999 * r)
+            ig = int(255.999 * g)
+            ib = int(255.999 * b)
+
+            print(f"{ir} {ig} {ib}\n")
+
+if __name__ == "__main__":
+    build_image()
